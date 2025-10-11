ba5857697fc80d0e0236d03a1952d490f9d3b660
FabricyoSilva <144567358+FabricyoSilva@users.noreply.github.com>
2025-10-11 15:42:00 -0300

    Update Home.jsx

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ml-4">
         {/* Tema*/}
         
         {/*Efeito do Background*/}
