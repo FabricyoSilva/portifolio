35d5ab20aa060eb5c4f72547ff11bf8e3396955d
FabricyoSilva <144567358+FabricyoSilva@users.noreply.github.com>
2025-12-27 16:11:59 -0300

    finalizando seção de habilidades

--- vite-project/src/components/SkillsSection.jsx
+++ vite-project/src/components/SkillsSection.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import {cn} from '@/lib/utils';
 const skills = [
     //Frontend
     {name: "HTML/CSS", level:95 , category:"frontend"},
@@ -25,6 +25,9 @@ const categories = ["all","frontend","backend","tools"]
 
 export const SkillsSection = () => {
     const [activeCategory,setActiveCategory] = useState("all");
+
+    const filteredSkills = skills.filter((skill)=> activeCategory === "all" || skill.category === activeCategory)
+
     return(
         <section id="skills" className="py-24 px-4 relative bg-secondary/30">
             <div className="container mx-auto max-w-5xl">
@@ -34,14 +37,16 @@ export const SkillsSection = () => {
 
                 <div className="flex flex-wrap justify-center gap-4 mb-12">
                     {categories.map((category,key)=> (
-                        <button key={key} className="px-5 py-2 rounded-full transition-colors duration-300 capitalize" onClick={()=> setActiveCategory(category)}>
+                        <button key={key} className={cn("px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                        activeCategory === category ? "bg-primary text-primary-foreground" : "bg-secondary/70 text-foreground hover:bg-secondary"
+                        )} onClick={()=> setActiveCategory(category)}>
                             {category}
                         </button>
                     ))}
                 </div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-                    {skills.map((skill, key)=> (
+                    {filteredSkills.map((skill, key)=> (
                         <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover">
                           <div className="text-left mb-4">
                             <h3 className="font-semibold text-lg">{skill.name}</h3>
