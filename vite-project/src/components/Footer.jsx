e755c3ca15b90d67e6794cb0813beb7ecceb6aee
Fabricyo Silva <fabricyoclash419@gmail.com>
2026-03-17 22:11:10 -0300

    Atualizar cor e footer do portifolio

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -4,11 +4,14 @@ export const Footer = () => {
     return (
         <>
             <footer className="py-12 px-4 bg-card border-t border-border mt-12 relative">
-                <div className="mt-8 border-t border-border pt-6">
+             
+                <div>
                     <p className="text-center text-sm text-muted-foreground">&copy; {new Date().getFullYear()} FabricyoSilva. Todos os direitos reservados.</p>
                 </div>
-                <a href="#hero" className="absolute bottom-6 right-6 p-3 rounded-full bg-primary text-white shadow-lg z-50 hover:scale-105 transition-transform">
-                    <ArrowUp size={20}/>
+
+                <a href="#hero" className="absolute flex items-center bottom-6 right-6 p-3 rounded-full bg-primary text-white shadow-lg z-50 hover:scale-105 transition-transform gap-1 px-4 py-2">
+                    <span className="text-sm font-medium">Voltar para o topo</span>
+                    <ArrowUp size={20} />
                 </a>
             </footer>
         </>
